d688467ecdb0257c6114071ecd20ca6408758418
widaksono <aguswidaksono@gmail.com>
2023-08-05 11:16:16 +0700

    Update Smartphone.java

--- src/main/java/UAS2/Smartphone.java
+++ src/main/java/UAS2/Smartphone.java
@@ -17,13 +17,13 @@ public class Smartphone implements Pajak {
     private String vendor;
     private String tipe;
     private double harga;
-
+//deklarasi
     public Smartphone(String vendor, String tipe, double harga) {
         this.vendor = vendor;
         this.tipe = tipe;
         this.harga = harga;
     }
-
+//konstruktor 
     @Override
     public double hitungHargaSetelahPPN(double harga) {
         if (harga > 4500000) {
@@ -36,7 +36,7 @@ public class Smartphone implements Pajak {
             return harga; // Tanpa PPN
         }
     }
-
+//kondisi diskon
     public void tampilkanHargaSetelahPPN() {
         double hargaSetelahPPN = hitungHargaSetelahPPN(this.harga);
         System.out.println("Harga smartphone " + vendor + " " + tipe + " setelah ditambah PPN: Rp " + hargaSetelahPPN);
